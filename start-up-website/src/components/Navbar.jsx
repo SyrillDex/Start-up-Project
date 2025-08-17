a21fffc0749d5757d08b78f46110cb0d4ccf0e13
syrillDex <syrilldexl@gmail.com>
2025-08-17 18:23:25 +0800

    navbar fixed

--- start-up-website/src/components/Navbar.jsx
+++ start-up-website/src/components/Navbar.jsx
@@ -4,17 +4,41 @@ import "../styles/Navbar.css";
 const Navbar = () => {
   return (
     <>
-      <div className="navbar container-fluid d-flex  justify-content-between align-items-center position-relative">
-        <ul className="navlinks d-flex justify-content-center align-items-center gap-3 p-0 text-white m-0">
-          <NavLink className="nav-item">Home</NavLink>
-          <NavLink className="nav-item">About Us</NavLink>
-          <NavLink className="nav-item">Location</NavLink>
-        </ul>
+      <nav className="navbar navbar-expand-lg container-fluid d-flex  justify-content-between align-items-center position-relative">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul
+            className="navlinks navbar-nav d-flex justify-content-center align-items-center gap-3 p-0 text-white m-0"
+            id="navbarSupportedContent"
+          >
+            <NavLink className="nav-item">
+              <a className="nav-link active" href="">Home</a>
+            </NavLink>
+            <NavLink className="nav-item">
+              <a className="nav-link">About Us</a>
+            </NavLink>
+            <NavLink className="nav-item">
+              <a className="nav-link">Location</a>
+            </NavLink>
+          </ul>
+        </div>
         <div className="sign-in-btn">
           <a href="#" className="btn btn-outline-light">
             sign in
           </a>
         </div>
+      </nav>
+      <div className="logo-div">
         <img
           className="logo rounded-circle position-absolute"
           src="src/assets/logo.jpg"
